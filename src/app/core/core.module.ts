b8641f202580128363fccc0b680d80d001382387
idkbutimlucas <76565606+idkbutimlucas@users.noreply.github.com>
2022-03-15 14:20:14 +0100

    Fixe error
    service and not found

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 import { StudentModule } from '../student/student.module';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, NotFoundComponent],
   imports: [CommonModule, SharedModule, StudentModule],
   exports: [HeaderComponent, FooterComponent],
 })
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { StudentRoutingModule } from './student-routing.module';
+import { StudentComponent } from './student.component';
+import { StudentListComponent } from './pages/student-list/student-list.component';
+import { StudentDetailsComponent } from './pages/student-details/student-details.component';
+import { StudentFormComponent } from './components/student-form/student-form.component';
+import { SharedModule } from '../shared/shared.module';
+import { StudentService } from './services/student.service';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, StudentRoutingModule],
+  declarations: [
+    StudentComponent,
+    StudentListComponent,
+    StudentDetailsComponent,
+    StudentFormComponent,
+  ],
+  imports: [CommonModule, StudentRoutingModule, SharedModule],
+  providers: [StudentService],
 })
 export class StudentModule {}
